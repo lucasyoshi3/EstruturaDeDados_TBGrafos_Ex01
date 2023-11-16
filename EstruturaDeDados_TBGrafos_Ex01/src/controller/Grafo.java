60837f3dceec4908c544cc103b0d34e2299f1f75
lucasyoshi3 <lucasyoshihirosm@gmail.com>
2023-11-15 23:49:29 -0300

    eConexo criado

--- EstruturaDeDados_TBGrafos_Ex01/src/controller/Grafo.java
+++ EstruturaDeDados_TBGrafos_Ex01/src/controller/Grafo.java
@@ -1,16 +1,20 @@
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class Grafo {
 
 	public void arestas(int[][] matriz, char[] alfabeto) {
 		// TODO Auto-generated method stub
 		int l=0;
 		int c=0;
-		while(l<5) {
+		while(l<matriz.length) {
 			if(matriz[l][c]==1) {
 				System.out.println("Aresta "+alfabeto[l]+" e "+alfabeto[c]);
 			}
-			if(c==4) {
+			if(c==matriz[0].length-1) {
 				l++;
 				c=-1;
 			}
@@ -37,9 +41,64 @@ public class Grafo {
 		}
 	}
 
-	public void grau(int[][] matriz) {
+	public void grau(int[][] matriz, char[] alfabeto) {
 		// TODO Auto-generated method stub
-		
+		int soma=0;
+		int i,j;
+		for(i=0;i<matriz.length;i++) {
+			for(j=0;j<matriz[i].length;j++) {
+				soma+=matriz[i][j];
+			}
+			System.out.println("Grau da vertice da "+alfabeto[i]+": "+soma);
+			soma=0;
+		}
+	}
+
+	public boolean eConexo(int[][] matriz, char[] alfabeto) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<matriz.length;i++) {
+			if(matriz[0][i]==1) {
+				break;
+			}
+			if(i==matriz.length-1) {
+				return false;
+			}
+		}
+		Stack<Integer> ordem= new Stack<Integer>();
+		List<Integer> visitados=new ArrayList<Integer>();
+		int tamanho=matriz.length;
+		ordem.push(0);
+		visitados.add(0);
+		while(!ordem.isEmpty()) {
+			
+			int vertice=ordem.peek();
+			boolean implementado=false;
+			
+			for(int j=0;j<tamanho;j++) {
+				if(matriz[vertice][j]==1 && !visitados.contains(j)) {
+					ordem.push(j);
+					visitados.add(j);
+					implementado=true;
+					break;
+				}
+			}
+			if(!implementado) {
+				ordem.pop();
+			}
+		}
+		for(int i=0;i<tamanho;i++) {
+			if(!visitados.contains(i)){
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
+
+
+
+
+
+
+	
--- EstruturaDeDados_TBGrafos_Ex01/src/view/Principal.java
+++ EstruturaDeDados_TBGrafos_Ex01/src/view/Principal.java
@@ -6,16 +6,20 @@ public class Principal {
 	public static void main(String[] args) {
 		Grafo grafo=new Grafo();
 		int[][] matriz = {
-			    {0, 1, 0, 1, 0},
-			    {1, 0, 1, 1, 1},
-			    {0, 1, 1, 0, 0},
-			    {1, 1, 0, 0, 1},
-			    {0, 1, 0, 1, 0}
+				{0, 1, 0, 0},
+			    {1, 0, 0, 0},
+			    {0, 0, 0, 1},
+			    {0, 0, 1, 0}
 			};
 		char[] alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
 		
 		grafo.arestas(matriz, alfabeto);
 		grafo.tipoAresta(matriz,alfabeto);
-		grafo.grau(matriz);
+		grafo.grau(matriz,alfabeto);
+		if(grafo.eConexo(matriz,alfabeto)) {
+			System.out.println("E conexo");
+		}else {
+			System.out.println("Não é conexo");
+		}
 	}
 }
